d09007ba8249af8535af78c0601d55ca60a195f2
Bruno Silva <brunnoborges@ufrn.edu.br>
2020-04-06 10:23:55 -0300

    dando forma a lista de tarefas [aula 7 -> 9 ]

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,15 @@ import 'models/item.dart';
 void main() => runApp(App());
 
 class App extends StatelessWidget {
-	@override
-	Widget build(BuildContext context) {
-		return MaterialApp(
-			title: 'Todo App',
-			debugShowCheckedModeBanner: false,
-			theme: ThemeData(primarySwatch: Colors.red),
-			home: HomePage(),
-		);
-	}
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: HomePage(),
+    );
+  }
 }
 
 //Sempre que seguir um padrão de pagina, usa-se o Scaffold (que se comporta como uma página) no return
@@ -22,13 +22,20 @@ class App extends StatelessWidget {
 class HomePage extends StatefulWidget {
   var items = new List<Item>();
 
-  HomePage(){
+  HomePage() {
     items = [];
-    items.add(Item(title: "Caminhada", done: true));
-    items.add(Item(title: "Compras", done: false));
-    items.add(Item(title: "Jogatina", done: true));
-    items.add(Item(title: "Dormida", done: true));
-    items.add(Item(title: "Almoço", done: true));
+    items.add(Item(
+        id: 1, title: "Caminhada", done: true, description: "Para fazer já!"));
+    items.add(Item(
+        id: 2,
+        title: "Amoloço",
+        done: true,
+        description: "Macarrão com arroz"));
+    items.add(Item(
+        id: 3,
+        title: "Lanche da tarde",
+        done: false,
+        description: "O resto do almoço"));
   }
 
   @override
@@ -36,19 +43,62 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-	@override
-	Widget build(BuildContext context) {
-		return Scaffold(
-			appBar: AppBar(
-				title: Text("Todo List")
-			),
-			body: ListView.builder(
-        itemCount: widget.items.length, //TAMANHO DA LISTA CRIADA NA CLASSE PAI
-        itemBuilder: (BuildContext ctxt, int index){
-          return Text(widget.items[index].title);
+  var newTask = TextEditingController();
+
+  void add() {
+    if (newTask.text.isEmpty) return;
+
+    setState(() {
+      widget.items.add(
+        Item(
+          title: newTask.text,
+          done: false,
+        ),
+      );
+      newTask.text = "";
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: TextFormField(
+          controller: newTask,
+          keyboardType: TextInputType.text,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 19,
+          ),
+          decoration: InputDecoration(
+            labelText: "Nova Tarefa",
+            labelStyle: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: widget.items.length,
+        itemBuilder: (BuildContext ctxt, int index) {
+          final item = widget.items[index];
+          return CheckboxListTile(
+            title: Text(item.title),
+            key: Key(item.title),
+            value: item.done,
+            onChanged: (value) {
+              setState(() {
+                item.done = value;
+              });
+            },
+          );
         },
       ),
-		);
-	}
+      floatingActionButton: FloatingActionButton(
+        onPressed: add,
+        child: Icon(Icons.add),
+        backgroundColor: Colors.red,
+      ),
+    );
+  }
 }
-
--- lib/models/item.dart
+++ lib/models/item.dart
@@ -1,17 +1,23 @@
 class Item {
+  int id;
   String title;
+  String description;
   bool done;
 
-  Item({this.title, this.done});
+  Item({this.title, this.done, int id, String description});
 
   Item.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
     title = json['title'];
+    description = json['description'];
     done = json['done'];
   }
 
   Map<String, dynamic> toJson(){
     final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
     data['title'] = this.title;
+    data['description'] = this.description;
     data['done'] = this.done;
     return data;
   }
